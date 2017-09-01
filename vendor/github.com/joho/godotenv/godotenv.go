fe651920256ff1976b9ddad13cfbeec04452dad6
Joachim Hill-Grannec <joachim@suvault.com>
2017-09-01 12:35:15 -0400

    switch to the standard govendor for drone-plugins

--- vendor/github.com/joho/godotenv/godotenv.go
+++ vendor/github.com/joho/godotenv/godotenv.go
@@ -119,15 +119,8 @@ func loadFile(filename string, overload bool) error {
 		return err
 	}
 
-	currentEnv := map[string]bool{}
-	rawEnv := os.Environ()
-	for _, rawEnvLine := range rawEnv {
-		key := strings.Split(rawEnvLine, "=")[0]
-		currentEnv[key] = true
-	}
-
 	for key, value := range envMap {
-		if !currentEnv[key] || overload {
+		if os.Getenv(key) == "" || overload {
 			os.Setenv(key, value)
 		}
 	}
@@ -150,19 +143,13 @@ func readFile(filename string) (envMap map[string]string, err error) {
 		lines = append(lines, scanner.Text())
 	}
 
-	if err = scanner.Err(); err != nil {
-		return
-	}
-
 	for _, fullLine := range lines {
 		if !isIgnoredLine(fullLine) {
-			var key, value string
-			key, value, err = parseLine(fullLine)
+			key, value, err := parseLine(fullLine)
 
-			if err != nil {
-				return
+			if err == nil {
+				envMap[key] = value
 			}
-			envMap[key] = value
 		}
 	}
 	return
@@ -219,23 +206,18 @@ func parseLine(line string) (key string, value string, err error) {
 
 	// Parse the value
 	value = splitString[1]
-
 	// trim
 	value = strings.Trim(value, " ")
 
 	// check if we've got quoted values
-	if value != "" {
-		first := string(value[0:1])
-		last := string(value[len(value)-1:])
-		if first == last && strings.ContainsAny(first, `"'`) {
-			// pull the quotes off the edges
-			value = strings.Trim(value, `"'`)
+	if strings.Count(value, "\"") == 2 || strings.Count(value, "'") == 2 {
+		// pull the quotes off the edges
+		value = strings.Trim(value, "\"'")
 
-			// expand quotes
-			value = strings.Replace(value, `\"`, `"`, -1)
-			// expand newlines
-			value = strings.Replace(value, `\n`, "\n", -1)
-		}
+		// expand quotes
+		value = strings.Replace(value, "\\\"", "\"", -1)
+		// expand newlines
+		value = strings.Replace(value, "\\n", "\n", -1)
 	}
 
 	return
